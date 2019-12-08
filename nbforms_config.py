406f026d1da0fa93179261fae66bd4b8c9476196
Chris Pyles <cpyles@berkeley.edu>
2019-12-08 14:18:20 -0800

    working on package

---
+++ nbforms_config.py
@@ -0,0 +1,35 @@
+config = {
+	"server_url": "http://data-88-nbforms.herokuapp.com",
+	"questions": [{
+		"identifier": "q1",
+		"type": "multiplechoice",
+		"question": "Which of the following is a noble gas?",
+		"options": [
+			"Helium",
+			"Chlorine",
+			"Oxygen",
+			"Lithium"
+		]
+	}, {
+		"identifier": "q2",
+		"type": "checkbox",
+		"question": "Which of the following is a noble gas?",
+		"options": [
+			"Helium",
+			"Chlorine",
+			"Oxygen",
+			"Lithium",
+			"Neon",
+			"Zync"
+		]
+	}, {
+		"identifier": "q3",
+		"type": "text",
+		"question": "Name a noble gas."
+	}, {
+		"identifier": "q4",
+		"type": "paragraph",
+		"question": "List the noble gases.",
+		"placeholder": "Type here"
+	}]
+}
